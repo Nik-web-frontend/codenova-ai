37ac06fa854b7840984b73bc8db9fd576ca562cf
Nikhare Yash <nikhareyash38@gmail.com>
2026-07-03 15:34:00 +0530

    Implement conversation flow in chat UI

--- client/src/pages/Chat.jsx
+++ client/src/pages/Chat.jsx
@@ -1,10 +1,10 @@
-import React from 'react'
+import React, { Fragment } from 'react'
 import './chat.css'
 import { useState } from 'react'
 
 const Chat = () => {
     const [prompt, setPrompt] = useState('')
-    const [reply, setReply] = useState('');
+    const [msgs, setMsgs] = useState([]);
     let [loading, setLoading] = useState(false);
 
     async function sendPrompt() {
@@ -15,6 +15,7 @@ const Chat = () => {
 
         try {
             setLoading(true);
+            setMsgs((prev) => [...prev, { role: "user", text: prompt }])
             const response = await fetch("http://localhost:5000/api/chat", {
                 method: "POST",
                 headers: {
@@ -28,12 +29,13 @@ const Chat = () => {
             if (!response.ok) {
                 throw new Error("Server error");
             }
+
             const data = await response.json();
-            setReply(data.response)
+            setMsgs((prev) => [...prev, { role: 'AI', text: data.response }])
             setPrompt('')
         }
         catch (error) {
-            setReply(error.message);
+            setMsgs((prev) => [...prev, { role: 'AI', text: error.message }])
         }
         finally {
             setLoading(false)
@@ -42,6 +44,7 @@ const Chat = () => {
 
     }
 
+
     return (
         <>
             <div className="navbar">
@@ -49,18 +52,26 @@ const Chat = () => {
             </div>
             <div className="prompt-container">
                 <div className="reply-container">
-                    {loading ? <p className='reply'>Loading...</p> : <p className='reply'>{reply}</p>}
+                    {msgs.map((data, idx) => (
+                        <Fragment key={idx}>
+                            <div className={`msg-wrap ${data.role === 'user' ? 'user-wrap':'ai-wrap'}`}>
+                                <p className={`reply ${data.role === 'user' ? 'user-text' : 'ai-text'}`}>{data.text}</p>
+                            </div>
+                        </Fragment>
+
+                    ))}
                 </div>
 
+                {loading && (
+                    <p className='reply'>Loading...</p>
+                )}
                 <div className="user-action">
                     <textarea placeholder='Ask Something...' value={prompt} onChange={(e) => {
                         setPrompt(e.target.value)
                     }}></textarea>
 
-                    <button onClick={() => {
-                        sendPrompt()
-                    }}
-                    disabled={loading || !prompt.trim()}
+                    <button onClick={sendPrompt}
+                        disabled={loading || !prompt.trim()}
                     >{loading ? "Generating..." : "send"}</button>
                 </div>
 
